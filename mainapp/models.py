8b2b6a0a239b7935c3256f4ef9076be2eb079231
Nikitos-art <kurpasnikita@gmail.com>
2021-09-18 13:46:37 +0300

    added model Product

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
