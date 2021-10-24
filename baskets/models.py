b015a6df65d33c297d89c91eb3c7be050ed83cfc
Nikitos-art <kurpasnikita@gmail.com>
2021-10-24 11:38:59 +0300

    менеджер, сигналы для обработки кол-ва товаров

--- baskets/models.py
+++ baskets/models.py
@@ -6,7 +6,17 @@ from mainapp.models import Product
 # Create your models here.
 
 
+# class BasketQuerySet(models.QuerySet):
+#
+#     def delete(self, *args, **kwargs):
+#         for item in self:
+#             item.product.quantity += item.quantity
+#             item.product.save()
+#         super(BasketQuerySet, self).delete()
+
+
 class Basket(models.Model):
+    # object = BasketQuerySet.as_manager()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
@@ -19,12 +29,28 @@ class Basket(models.Model):
     def sum(self):
         return self.quantity * self.product.price
 
-    @staticmethod
     def total_sum(user):
         baskets = Basket.objects.filter(user=user)
         return sum(basket.sum() for basket in baskets)
 
-    @staticmethod
     def total_quantity(user):
         baskets = Basket.objects.filter(user=user)
         return sum(basket.quantity for basket in baskets)
+
+    # def delete(self, using=None, keep_parents=False):
+    #     self.product.quantity += self.quantity
+    #     self.product.save()
+    #     super(Basket, self).delete()
+    #
+    # def save(self, force_insert=False, force_update=False, using=None,
+    #          update_fields=None):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - self.get_item(int(self.pk))
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(Basket, self).save()
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
